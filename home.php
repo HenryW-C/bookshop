cc473de933b4c17e729b73196c4001c6f2bb31ba
HWoodCollins <114936314+HWoodCollins@users.noreply.github.com>
2023-05-10 09:44:53 +0100

    P1

--- home.php
+++ home.php
@@ -3,26 +3,37 @@
 <head>
     
     <title>Template</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- links to stylesheets and google fonts -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="stylesheet.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto Slab">
     
 </head>
 <body>
     
+    <!-- top navbar -->
     <div class="navbar_top">
-        <a class="logo" class="button" href="#home">Bella's<br>Books</a>
-        <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-primary">Basket</button>
-            <button type="button" class="btn btn-primary">My Account</button>
-            <button type="button" class="btn btn-primary">Sign In</button>
-        </div> 
+        <div class="container-fluid">
+            
+            <a class="logo" class="button" href="#home">Bella's<br>Books</a>
+
+            <div class="float-end">
+                <div  class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-primary">Basket</button>
+                    <button type="button" class="btn btn-primary">My Account</button>
+                    <button type="button" class="btn btn-primary">Sign In</button>
+                </div> 
+            </div> 
+        </div>
     </div>
 
+    <!-- body of website -->
     <div class="main">
-        <h1>Body</h1>
+        <p>Body</p>
     </div> 
 
+    <!-- bottom navbar -->
     <div class="navbar_bottom">
         &nbsp
     </div>
