83931a180974a528893dfecf5f45bef8bd68fccb
HWoodCollins <114936314+HWoodCollins@users.noreply.github.com>
2023-08-28 15:15:54 +0100

    removed aria and redundant jsquey

--- homepage.php
+++ homepage.php
@@ -9,7 +9,6 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="jquery-3.6.4.min.js"></script>
     <link rel="stylesheet" type="text/css" href="stylesheet.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto Slab">
     
@@ -26,17 +25,17 @@
             <div class="search-bar">
                 <form class="d-flex" id="searchForm">
                     <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
                             <span id="selectedOption">Categories</span>
                         </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <div class="dropdown-menu">
                             <a class="dropdown-item" href="#" data-value="Option 1">Option 1</a>
                             <a class="dropdown-item" href="#" data-value="Option 2">Option 2</a>
                             <a class="dropdown-item" href="#" data-value="Option 3">Option 3</a>
                         </div>
                     </div>
                     <input type="hidden" id="selectedValue" name="selectedValue">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <input class="form-control me-2" type="search" placeholder="Search">
                     <button class="btn btn-outline-success go-button" type="submit">Go</button>
                 </form>
             </div>
