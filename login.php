8f14104771f9011038838cbb35155863943871ec
HWoodCollins <henrywoodcollins@icloud.com>
2023-09-01 21:26:08 +0100

    added favicon

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
     
     <title>Login</title>
     <!-- links to stylesheets and google fonts -->
+    <link rel="icon" type="image/x-icon" href="images/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="stylesheet.css">
