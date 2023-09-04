8f9d75b3b2c89dae8ccd404051dc42451f0bfe7e
HWoodCollins <114936314+HWoodCollins@users.noreply.github.com>
2023-09-04 09:44:50 +0100

    added checkout page

---
+++ checkout.php
@@ -0,0 +1,19 @@
+<?php
+include_once("connection.php");
+session_start(); 
+
+$date = date("Y-m-d h:m:s");
+
+$stmt = $conn->prepare("INSERT INTO 
+    TblOrders (orderID,totalPrice,orderDate,userID)
+    VALUES (null,:price,:date,:userID)");
+
+    $stmt->bindParam(':price', $_SESSION['totalPrice']);
+    $stmt->bindParam(':date', $date);
+    $stmt->bindParam(':userID', $_SESSION["UserID"]);
+    $stmt->execute();
+    $_SESSION['totalPrice']=0;
+
+// sends the user to the homepage
+header('Location: homepage.php');
+?>
