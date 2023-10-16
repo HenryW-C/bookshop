8e39becf56e72fea59ec7993e02d2c2cc4c17e75
HWoodCollins <114936314+HWoodCollins@users.noreply.github.com>
2023-10-16 09:41:27 +0100

    finished order account page and started order page

--- admin_account.php
+++ admin_account.php
@@ -102,7 +102,40 @@ if (!isset($_SESSION['Email'])) {
                 <div class="col-md-4">
                     <div class="custom-column">
                         <h3>Past Orders</h3>
-                        <p>[Past Orders]</p>
+                        <?php
+                            // fetch all orders that the user has made
+                            $stmt = $conn->prepare("SELECT * FROM tblOrders WHERE userID = :userID");
+                            $stmt->bindParam(':userID', $_SESSION['UserID'], PDO::PARAM_INT);
+                            $stmt->execute();
+                            $orderData = $stmt->fetch(PDO::FETCH_ASSOC);
+                            // sets variable to determine if there are orders
+                            if ($orderData) {
+                                $orderFull = 1;
+                            }
+                            else {
+                                $orderFull = 0;
+                            }
+                            if ($orderFull == 1) {
+                                // if there are orders, they are displayed
+                                do {
+                                    // row to fill the width of the page
+                                    echo ('<a class="link" href="order.php?orderID=' .$orderData["orderID"]. '" class="card-title">'); // link to order page
+                                        echo('<div class="custom-row">');
+                                            echo ('<div class="title">');
+                                                echo('<h4>Order #'.$orderData["orderID"].'</h4>');
+                                            echo ('</div>');
+                                            echo ('<div class="price">');
+                                                $price = number_format($orderData['totalPrice'], 2, '.', ',');
+                                                echo('<h4>£'.$price.'</h4>');
+                                            echo ('</div>');
+                                        echo('</div>');
+                                    echo ('</a>');
+                                } while ($orderData = $stmt->fetch(PDO::FETCH_ASSOC));
+                            } else {
+                                // message to be shown if there are no orders
+                                echo('<br>There are no past orders');
+                            }
+                        ?>
                     </div>
                 </div>
                 <div class="col-md-4">
--- order.php
+++ order.php
@@ -1,14 +1,8 @@
 <?php
-// link to connection.php to access the database
 include_once("connection.php");
 
-// starts session and ensures that user is logged in as customer, if not, they are sent to login
+// starts session and ensures that user is logged in, if not, they are sent to login
 session_start(); 
-$_SESSION['backURL']='customer_account.php';
-if ($_SESSION['UserType'] != 0) {
-    $_SESSION['Message'] = "Please login to use this service";
-    header('Location: login.php');
-  }
 if (!isset($_SESSION['Email'])) {
   $_SESSION['Message'] = "Please login to use this service";
   header('Location: login.php');
@@ -18,19 +12,23 @@ if (!isset($_SESSION['Email'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Manage Account</title>
+    
+    <title>Order</title>
     <!-- links to stylesheets and google fonts -->
     <link rel="icon" type="image/x-icon" href="images/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto Slab">
-
+    
 </head>
 <body>
+    
     <!-- top navbar -->
+    <div class="white_box_top"></div>
     <div class="navbar_top">
         <div class="container-fluid">
+
             <!-- logo and link to homepage -->
             <a class="logo" type="button" href="/bookshop/homepage.php">Bella's<br>Books</a>
 
@@ -46,7 +44,6 @@ if (!isset($_SESSION['Email'])) {
                             <optgroup label="Levels">
                                 <!-- php code to take levels from table and display as dropdown options -->
                                 <?php
-                                session_start(); 
                                 include_once ("connection.php");
                                 $stmt = $conn->prepare('SELECT category FROM tblCategories WHERE categoryType = 0');
                                 $stmt->execute();
@@ -76,12 +73,25 @@ if (!isset($_SESSION['Email'])) {
                 </form>
             </div>
 
+
             <!-- buttons at end of navbar -->
             <span>
                 <div class="float-end">
                     <div  class="btn-group" role="group">
+                        <!-- php switch statement to show select buttons dependant on user type -->
+                        <?php
+                        switch ($_SESSION['UserType']) { 
+                        case(1): ?>
+                        <a type="button" href="/bookshop/admin_account.php" class="btn btn-primary">My Account</a>
                         <a type="button" href="/bookshop/basket.php" class="btn btn-primary">Basket</a>
                         <a type="button" href="/bookshop/logout.php" class="btn btn-primary">Logout</a>
+                        <?php break; ?>
+
+                        <?php case(0): ?>
+                        <a type="button" href="/bookshop/customer_account.php" class="btn btn-primary">My Account</a>
+                        <a type="button" href="/bookshop/basket.php" class="btn btn-primary">Basket</a>
+                        <a type="button" href="/bookshop/logout.php" class="btn btn-primary">Logout</a>
+                        <?php break; }?>
                     </div> 
                 </div> 
             </span>
@@ -89,62 +99,48 @@ if (!isset($_SESSION['Email'])) {
     </div>
 
     <!-- body of website -->
-    <div class="main">
-        <div class="container-fluid mt-5"> 
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="custom-column">
-                        <h3>Delivery Details</h3>
-                        <p>[Delivery Details]</p>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="custom-column">
-                        <h3>Past Orders</h3>
-                        <?php
-                            // fetch all orders that the user has made
-                            $stmt = $conn->prepare("SELECT * FROM tblOrders WHERE userID = :userID");
-                            $stmt->bindParam(':userID', $_SESSION['UserID'], PDO::PARAM_INT);
-                            $stmt->execute();
-                            $orderData = $stmt->fetch(PDO::FETCH_ASSOC);
-                            // sets variable to determine if there are orders
-                            if ($orderData) {
-                                $orderFull = 1;
-                            }
-                            else {
-                                $orderFull = 0;
-                            }
-                            if ($orderFull == 1) {
-                                // if there are orders, they are displayed
-                                do {
-                                    // row to fill the width of the page
-                                    echo ('<a class="link" href="order.php?orderID=' .$orderData["orderID"]. '" class="card-title">'); // link to order page
-                                        echo('<div class="custom-row">');
-                                            echo ('<div class="title">');
-                                                echo('<h4>Order #'.$orderData["orderID"].'</h4>');
-                                            echo ('</div>');
-                                            echo ('<div class="price">');
-                                                $price = number_format($orderData['totalPrice'], 2, '.', ',');
-                                                echo('<h4>£'.$price.'</h4>');
-                                            echo ('</div>');
-                                        echo('</div>');
-                                    echo ('</a>');
-                                } while ($orderData = $stmt->fetch(PDO::FETCH_ASSOC));
-                            } else {
-                                // message to be shown if there are no orders
-                                echo('<br>There are no past orders');
-                            }
-                        ?>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="order">
+        <?php
+            include_once("connection.php");
+            if (!isset($_SESSION['Email'])) {
+            $_SESSION['Message'] = "Please login to use this service";
+            header('Location: login.php');
+            }
+            
+            array_map("htmlspecialchars", $_GET);
+            // retrieve the orderID from the query parameter
+            if (isset($_GET['orderID']) && !empty($_GET['orderID'])) {
+                $orderID = $_GET['orderID'];
+                
+                // fetch order data from tblBooks using the orderID
+                $stmt = $conn->prepare("SELECT * FROM tblBooks WHERE orderID = :orderID");
+                $stmt->bindParam(':orderID', $orderID, PDO::PARAM_INT);
+                
+                try {
+                    $stmt->execute();
+                    $orderData = $stmt->fetch(PDO::FETCH_ASSOC);
+                    
+                    // display book details
+                    if ($orderData) {
+                        // CODE TO DISPLAY BOOKS
+                        
+                    } else {
+                        echo ('Order not found');
+                    }
+                } catch (PDOException $e) {
+                    echo ("Error: " . $e->getMessage());
+                }
+            } else {
+                echo ('Invalid orderID');
+            }
+        ?>
     </div> 
 
     <!-- bottom navbar -->
+    <div class="white_box_bottom"><div>
     <div class="navbar_bottom">
         <a> </a>
     </div>
-   
+
 </body>
 </html>
