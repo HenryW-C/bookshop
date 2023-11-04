814c1f39a3b41865953415eac4184f160aaaeaf5
HWoodCollins <henrywoodcollins@icloud.com>
2023-11-04 12:40:19 +0000

    removed empty space

--- upload.php
+++ upload.php
@@ -26,33 +26,33 @@ if (isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"]["error"] === 0) {
 
   // check if file already exists
   if (file_exists($target_file)) {
-      echo "Sorry, file already exists.";
-      $uploadOk = 0;
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
   }
 
   // check file size
   if ($_FILES["fileToUpload"]["size"] > 500000) {
-      echo "Sorry, your file is too large.";
-      $uploadOk = 0;
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
   }
 
   // check if $uploadOk is set to 0 by an error
   if ($uploadOk == 0) {
-      echo "Sorry, your file was not uploaded.";
+    echo "Sorry, your file was not uploaded.";
   } else {
-      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-          echo "The file " . htmlspecialchars($filename) . " has been uploaded.";
-          header('Location: ' . $backURL);
-      } else {
-          echo "Sorry, there was an error uploading your file.";
-      }
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file " . htmlspecialchars($filename) . " has been uploaded.";
+      header('Location: ' . $backURL);
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+    }
   }
 
-    // tblBooks is updated to list the new file name
-    $stmt = $conn->prepare("UPDATE tblBooks SET image=:imageName WHERE bookID=:bookID");
-    $stmt->bindParam(':imageName', $filename);
-    $stmt->bindParam(':bookID', $conn->lastInsertId());
-    $stmt->execute();
-    $conn=null;
+  // tblBooks is updated to list the new file name
+  $stmt = $conn->prepare("UPDATE tblBooks SET image=:imageName WHERE bookID=:bookID");
+  $stmt->bindParam(':imageName', $filename);
+  $stmt->bindParam(':bookID', $conn->lastInsertId());
+  $stmt->execute();
+  $conn=null;
 }
 ?>
