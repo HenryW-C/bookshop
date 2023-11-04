4d219cf3dc23e3f641cc609051beba1c512716d1
HWoodCollins <henrywoodcollins@icloud.com>
2023-11-04 12:41:39 +0000

    removed invalid

--- upload.php
+++ upload.php
@@ -24,12 +24,6 @@ if (isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"]["error"] === 0) {
   $uploadOk = 1;
   $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-  // check if file already exists
-  if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-  }
-
   // check file size
   if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
